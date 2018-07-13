5bdd493e4d92a5b98d742e25b7207a21f2fcaa6f
JLasc <lascaze.j@gmail.com>
2018-07-13 15:52:50 -0400

    created globals & added questions/answers

---
+++ assets/javascript/app.js
@@ -0,0 +1,67 @@
+
+/* $( document ).ready(function() { */
+   
+
+//Globals
+var timeRemaining = 0;
+var correctGuess = 0;
+var wrongGuess = 0;
+var intervalId;
+
+
+//Arrays
+
+
+//Question Objects
+
+var questionObj = {
+first: "Who masterminded the plot to kill King Joffrey?",
+second: "What is Brienne's real surname?",
+third: "Grey Worm is the leader of which group?",
+fourth: "What is the official Lannister family motto?",
+fifth: "Who was burned alive on Drogo's funeral pyre?",
+sixth: "Who said, 'Of all the ways to kill you, poison would be the last'?",
+seventh: "Who said, 'Hush, Hodor! No more Hodoring!",
+eigth: "How is 'The Queen of Thorns' more commonly known?",
+ninth: "What is the name of Jon Snow's Direwolf?",
+tenth: "Which family does this crest belong to?"
+};
+
+var answerObj = {
+first: ["Little mix", "Littlefinger", "Olenna Tyrell", "Rene Artois"], /* answer: Littlefinger */
+second: ["Tully", "Tarth", "Gibb", "Utichi"], /* answer: Tarth */
+third: ["The Unthanks", "The Unjust", "The Unsullied", "The Undertones"], /* answer: The Unsullied */
+fourth: ['"Hear me roar"', '"A Lannister always pays his debts"', '"None so fierce"', '"Never knowingly undersold"'], /* answer: Lannister always pays debts */
+fifth: ["Mirri Maz Duur", "Khal Drogo", "Maris Piper", "Septa Mordane"], /* answer: Mirri Maz Duur */
+sixth: ["Mance Raydar", "Littlefinger", "Bowfinger", "Crast"], /* answer: Mance Raydar */
+seventh: ["Bran Stark", "Lord Varys", "Cersei Lannister", "Portmeirion"], /* answer: Bran Stark */
+eight: ["Cersei Lannister", "Margaery Tyrell", "Olenna Tyrell", "Jarvis Thribb"], /* answer: Olenna Tyrell */
+ninth: ["Grey Wind", "Graham", "Ghost", "Gargamel"], /* answer: Ghost */
+tenth: ["Kardashian", "Targaryen", "Tully", "Martell"] /* answer: Targaryen */
+};
+
+//Functions
+
+var start = function() {
+
+};
+
+var nextQuestion = function() {
+
+};
+
+
+var reset = function() {
+
+};
+
+
+
+
+//Click events -- add later, will return undefined with doc.ready commented
+
+
+
+
+
+/* });  */
